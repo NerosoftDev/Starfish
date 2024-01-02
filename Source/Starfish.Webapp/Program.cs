beb16d6a200ac5b917aaa9935d06751eb5b5ce23
damon <zhaorong@outlook.com>
2024-01-02 21:33:03 +0800

    引入FluentUI Components

--- Source/Starfish.Webapp/Program.cs
+++ Source/Starfish.Webapp/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.FluentUI.AspNetCore.Components;
 
 namespace Nerosoft.Starfish.Webapp;
 public class Program
@@ -9,6 +10,8 @@ public class Program
 		var builder = WebAssemblyHostBuilder.CreateDefault(args);
 		builder.RootComponents.Add<App>("#app");
 		builder.RootComponents.Add<HeadOutlet>("head::after");
+
+		builder.Services.AddFluentUIComponents();
 
 		builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
