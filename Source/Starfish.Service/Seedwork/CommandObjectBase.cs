0cd089bfc590791526ef9d181f31de91954e6d69
damon <zhaorong@outlook.com>
2024-02-06 01:14:19 +0800

    增加CommandObjectBase

---
+++ Source/Starfish.Service/Seedwork/CommandObjectBase.cs
@@ -0,0 +1,18 @@
+﻿using Nerosoft.Euonia.Business;
+using Nerosoft.Euonia.Claims;
+
+namespace Nerosoft.Starfish.Service;
+
+public abstract class CommandObjectBase<T> : CommandObject<T>, IHasLazyServiceProvider
+	where T : CommandObjectBase<T>
+{
+	/// <summary>
+	/// 懒加载服务提供程序
+	/// </summary>
+	public ILazyServiceProvider LazyServiceProvider { get; set; }
+
+	/// <summary>
+	/// 当前授权用户信息
+	/// </summary>
+	protected virtual UserPrincipal Identity => LazyServiceProvider.GetRequiredService<UserPrincipal>();
+}
